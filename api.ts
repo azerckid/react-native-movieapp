ab3e9c3e15ab7eba77c0e2a6fd2620d113740586
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-07-04 16:41:33 +0900

    1. search screen

--- api.ts
+++ api.ts
@@ -1,3 +1,5 @@
+import { QueryFunction } from "react-query";
+
 const API_KEY = "c39f66305cb6bd411613ca999ce32f56";
 const BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,6 +19,99 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: object;
+  budget: number;
+  genres: object;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object;
+  production_countries: object;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: object;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  videos: {
+    results: {
+      name: string;
+      key: string;
+      site: string;
+    }[];
+  };
+  images: object;
+}
+
+export interface TV {
+  name: string;
+  original_name: string;
+  origin_country: string[];
+  vote_count: number;
+  backdrop_path: string | null;
+  vote_average: number;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  poster_path: string | null;
+  first_air_date: string;
+  popularity: number;
+  media_type: string;
+}
+export interface TVDetails {
+  backdrop_path: string;
+  created_by: object;
+  episode_run_time: object;
+  first_air_date: string;
+  genres: object;
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: object;
+  last_air_date: string;
+  last_episode_to_air: object;
+  name: string;
+  next_episode_to_air: object;
+  networks: object;
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: object;
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object;
+  production_countries: object;
+  seasons: object;
+  spoken_languages: object;
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+  videos: {
+    results: {
+      name: string;
+      key: string;
+      site: string;
+    }[];
+  };
+  images: object;
+}
 
 interface BaseResponse {
   page: number;
@@ -27,20 +122,79 @@ interface BaseResponse {
 export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
+export interface TVResponse extends BaseResponse {
+  results: TV[];
+}
 
-const trending = () =>
-  fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
-    res.json()
-  );
+type MovieListResponse = QueryFunction<MovieResponse>;
+type TVListResponse = QueryFunction<TVResponse>;
 
-const upcoming = () =>
-  fetch(
-    `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
-  ).then((res) => res.json());
+interface MovieFetchers {
+  trending: MovieListResponse;
+  upcoming: MovieListResponse;
+  nowPlaying: MovieListResponse;
+  search: MovieListResponse;
+  detail: QueryFunction<MovieDetails>;
+}
 
-const nowPlaying = () =>
-  fetch(
-    `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
-  ).then((res) => res.json());
+interface TVFetchers {
+  trending: TVListResponse;
+  airingToday: TVListResponse;
+  topRated: TVListResponse;
+  search: TVListResponse;
+  detail: QueryFunction<TVDetails>;
+}
 
-export const moviesApi = { trending, upcoming, nowPlaying };
+export const moviesApi: MovieFetchers = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  upcoming: ({ pageParam }) =>
+    fetch(
+      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${pageParam}`
+    ).then((res) => res.json()),
+  nowPlaying: () =>
+    fetch(
+      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+    ).then((res) => res.json()),
+  search: ({ queryKey }) => {
+    const [_, query] = queryKey;
+    return fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+    ).then((res) => res.json());
+  },
+};
+
+export const tvApi: TVFetchers = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  airingToday: () =>
+    fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  topRated: () =>
+    fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
+      res.json()
+    ),
+  search: ({ queryKey }) => {
+    const [_, query] = queryKey;
+    return fetch(
+      `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+    ).then((res) => res.json());
+  },
+};
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,39 +1,26 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import { ActivityIndicator, Dimensions, FlatList } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
+import Loader from "../components/Loader";
 import Slide from "../components/Slide";
+import HList from "../components/HList";
 import HMedia from "../components/HMedia";
 import VMedia from "../components/VMedia";
 import { moviesApi, MovieResponse } from "../api";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Loader = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
 const ListTitle = styled.Text`
   color: black;
   font-size: 18px;
   font-weight: 600;
   margin-left: 30px;
 `;
-
-const TrendingScroll = styled.FlatList`
-  margin-top: 20px;
-`;
-const ListContainer = styled.View`
-  margin-bottom: 40px;
-`;
 const ComingSoonTitle = styled(ListTitle)`
   margin-bottom: 20px;
-`;
-const VSeparator = styled.View`
-  width: 20px;
 `;
 const HSeparator = styled.View`
   height: 20px;
@@ -41,35 +28,24 @@ const HSeparator = styled.View`
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const queryClient = useQueryClient();
-  const {
-    isLoading: nowPlayingLoading,
-    data: nowPlayingData,
-    isRefetching: isRefetchingNowPlaying,
-  } = useQuery<MovieResponse>(["movies", "nowPlaying"], moviesApi.nowPlaying);
-  const {
-    isLoading: upcomingLoading,
-    data: upcomingData,
-    isRefetching: isRefetchingUpcoming,
-  } = useQuery<MovieResponse>(["movies", "upcoming"], moviesApi.upcoming);
-  const {
-    isLoading: trendingLoading,
-    data: trendingData,
-    isRefetching: isRefetchingTrending,
-  } = useQuery<MovieResponse>(["movies", "trending"], moviesApi.trending);
+  const [refreshing, setRefreshing] = useState(false);
+  const { isLoading: nowPlayingLoading, data: nowPlayingData } =
+    useQuery<MovieResponse>(["movies", "nowPlaying"], moviesApi.nowPlaying);
+  const { isLoading: upcomingLoading, data: upcomingData } =
+    useQuery<MovieResponse>(["movies", "upcoming"], moviesApi.upcoming);
+  const { isLoading: trendingLoading, data: trendingData } =
+    useQuery<MovieResponse>(["movies", "trending"], moviesApi.trending);
 
   const onRefresh = async () => {
-    queryClient.refetchQueries(["movies"]);
+    setRefreshing(true);
+    await queryClient.refetchQueries(["movies"]);
+    setRefreshing(false);
   };
 
   const loading = nowPlayingLoading || upcomingLoading || trendingLoading;
-  const refreshing =
-    isRefetchingNowPlaying || isRefetchingUpcoming || isRefetchingTrending;
-  console.log(refreshing);
 
   return loading ? (
-    <Loader>
-      <ActivityIndicator />
-    </Loader>
+    <Loader />
   ) : upcomingData ? (
     <FlatList
       onRefresh={onRefresh}
@@ -100,27 +76,9 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               />
             ))}
           </Swiper>
-          <ListContainer>
-            <ListTitle>Trending Movies</ListTitle>
-            {trendingData ? (
-              <FlatList
-                style={{ marginTop: 20 }}
-                horizontal
-                data={trendingData.results}
-                keyExtractor={(item) => item.id + ""}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ paddingHorizontal: 30 }}
-                ItemSeparatorComponent={VSeparator}
-                renderItem={({ item }) => (
-                  <VMedia
-                    posterPath={item.poster_path || ""}
-                    originalTitle={item.original_title}
-                    voteAverage={item.vote_average}
-                  />
-                )}
-              />
-            ) : null}
-          </ListContainer>
+          {trendingData ? (
+            <HList title="Trending Movies" data={trendingData.results} />
+          ) : null}
           <ComingSoonTitle>Coming soon</ComingSoonTitle>
         </>
       }
